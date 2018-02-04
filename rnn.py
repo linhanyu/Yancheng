6d5c9990462124b44f389fb952802d618b897c4e
henrylin <305991071@qq.com>
2018-02-04 09:54:58 +0800

    修正

--- rnn.py
+++ rnn.py
@@ -8,7 +8,8 @@ import keras
 
 def build_part1_RNN(window_size):
     model = Sequential()
-    model.add(LSTM(7, input_shape=(window_size, 1)))
+    model.add(LSTM(window_size, input_shape=(window_size, 1)))
+    model.add(LSTM(window_size)
     model.add(Dense(1))
 
     return model
